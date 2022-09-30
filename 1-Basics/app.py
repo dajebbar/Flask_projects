1cc3f1723fe3615a7d6088a21b59c0135c235ae7
dajebbar <dajebbar@gmx.fr>
2022-09-30 15:07:50 +0100

    request query string

--- 1-Basics/app.py
+++ 1-Basics/app.py
@@ -1,5 +1,4 @@
-from email.policy import default
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -15,13 +14,24 @@ def json():
         'age' : 4,
         'lang': ['french', 'english'],
     }
-    return f'<h3>Infos:</h3>\n{x}'
+    return f'<h3>Infos:</h3>\n{jsonify(x)}'
 
 
 @app.route('/contact', methods=['GET', 'POST'], defaults={'name': 'Ali-Riad'})
 @app.route('/contact/<string:name>', methods=['GET', 'POST'])
 def contact(name):
     return f'Hello {name}! You are in the contact page.'
+
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    name = request.args.get('name')
+    location = request.args.get('loc')
+    ip = request.args.get('ip')
+    return (
+        f'<h3>Hello {name}! Your are from {location}. You are in a query page</h3>'
+        f'<h2>your Ip adress is {ip}<h2>'
+    )
+
 
 if __name__=='__main__':
     app.run(debug=True)
