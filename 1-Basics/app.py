1e5a4f3ccd56fb0d276f231bfad687e9aebe1660
dajebbar <dajebbar@gmx.fr>
2022-09-30 14:36:07 +0100

    app route methods get post

--- 1-Basics/app.py
+++ 1-Basics/app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def index():
     return '<h3>Main page</h3>'
 
-@app.route('/json')
+@app.route('/json', methods=['POST', 'GET'])
 def json():
     x = {
         'firstname': 'ali-riad',
         'lastname': 'boubekri',
-        'age' : 5,
-        'lang': ['arabic', 'french', 'english'],
+        'age' : 4,
+        'lang': ['french', 'english'],
     }
     return f'<h3>Infos:</h3>\n{x}'
 
