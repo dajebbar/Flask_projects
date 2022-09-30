d6ec5a6ba49de1855857bc4e426f3294b7259606
dajebbar <dajebbar@gmx.fr>
2022-09-30 14:52:24 +0100

    route variables

--- 1-Basics/app.py
+++ 1-Basics/app.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -16,6 +17,11 @@ def json():
     }
     return f'<h3>Infos:</h3>\n{x}'
 
+
+@app.route('/contact', methods=['GET', 'POST'], defaults={'name': 'Ali-Riad'})
+@app.route('/contact/<string:name>', methods=['GET', 'POST'])
+def contact(name):
+    return f'Hello {name}! You are in the contact page.'
 
 if __name__=='__main__':
     app.run(debug=True)
